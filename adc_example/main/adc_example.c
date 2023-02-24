3bbf0393b817156c0e21283f1bc1c1cb7f467bd7
Guillermo Limón <gmlimon@gmail.com>
2023-02-24 13:32:53 -0800

    fourth commit

--- adc_example/main/adc_example.c
+++ adc_example/main/adc_example.c
@@ -2,23 +2,23 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "driver/adc.h"
+#include "driver/adc.h" //Librería para el ADC
 
 #define ADC1_4 ADC1_CHANNEL_4
 
-//Subroutine declaration
+//Declaración de la subrutina de configuración
 void configADC(void);
 
-int val_pot = 0;
+int val_pot = 0; //Declaro e inicializo la variable de lectura para el ADC
 
 void app_main(void)
 
 {
     while(1)
     {
-        val_pot = adc1_get_raw(ADC1_4);
-        printf("ADC reading = %i\n", val_pot);
-        vTaskDelay(300/portTICK_PERIOD_MS);
+        val_pot = adc1_get_raw(ADC1_4); //Instrucción para leer el canal del ADC
+        printf("ADC reading = %i\n", val_pot); //Imprimo el resultado
+        vTaskDelay(300/portTICK_PERIOD_MS); //Retardo de 300 ms al mostrar los mensajes
     }
 
 }
